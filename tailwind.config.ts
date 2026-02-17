5afcecbe402f26612f3e38a2fec72a31eddb0b0b
Yassin <vedoxmorocco@gmail.com>
2026-02-17 00:57:27 +0000

    fix netlify config

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./index.html",
+    "./App.tsx",
+    "./index.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
